dcfedb6affc27bfb4a78df5f9c2dbb4337d58dc7
RHsanto <rakibulsanto789@gmail.com>
2022-07-20 14:39:31 +0600

    add

---
+++ app.js
@@ -0,0 +1,40 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+require('dotenv').config();
+const mongoose = require("mongoose")
+const path = require("path")
+
+
+const app = express();
+
+// process.env.APP_NAME
+
+// database connection
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+
+.then(()=>console.log("Connection successful"))
+.catch(err =>console.log(err))
+
+// request parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+//set view engine
+app.set("view engine","ejs")
+
+//set static folder
+app.use(express.static(path.join(__dirname,"public")))
+
+//pars cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing set up
+
+//error handling
+
+app.listen(process.env.PORT,()=>{
+  console.log(`app listing to port${process.env.PORT}`);
+})
