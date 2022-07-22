3f7255b61630c455f4bc8c0f1fccdea81a637546
RHsanto <rakibulsanto789@gmail.com>
2022-07-23 00:15:52 +0600

    ad

--- middleware/common/errorHandler.js
+++ middleware/common/errorHandler.js
@@ -7,7 +7,9 @@ function notFoundHandler(req,res,next){
 
 // default error handler
 function errorHandler(err,req,res,next){
-res.locals.error = 
+res.locals.error = process.env.NODE_ENV == 'development' ? err : {message:err.message}
+res.status(err.status || 500);
+
 }
 
 module.exports= {
