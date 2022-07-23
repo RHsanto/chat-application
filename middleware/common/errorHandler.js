c24b5c32634ff35c007e89850b059f055a7fd10a
RHsanto <rakibulsanto789@gmail.com>
2022-07-23 16:24:50 +0600

    add

--- middleware/common/errorHandler.js
+++ middleware/common/errorHandler.js
@@ -4,7 +4,7 @@ const createError =require("http-errors")
 function notFoundHandler(req,res,next){
   next(createError(404, "Your requested content was not found" ));
 }
-        
+              
 // default error handler
 function errorHandler(err,req,res,next){
 res.locals.error = process.env.NODE_ENV == 'development' ? err : {message:err.message}
